c6da4645dc0de9c7b815f271914c64bc0ee32d68
Vovashprot <vova.protasenko@gmail.com>
2023-08-16 00:46:43 +0300

    start

---
+++ src/main/java/com/example/TempPaper2/TempPaper2Application.java
@@ -0,0 +1,13 @@
+package com.example.TempPaper2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempPaper2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempPaper2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TempPaper2/TempPaper2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TempPaper2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempPaper2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
